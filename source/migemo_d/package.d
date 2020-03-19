df853766f813f8af1ed732c2c8efbcad4722b458
dokutoku <3729541-dokutoku@users.noreply.gitlab.com>
2020-03-19 14:14:18 +0900

    Add package.d

---
+++ source/migemo_d/package.d
@@ -0,0 +1,11 @@
+module migemo_d;
+
+
+public import migemo_d.charset;
+public import migemo_d.filename;
+public import migemo_d.migemo;
+public import migemo_d.mnode;
+public import migemo_d.romaji;
+public import migemo_d.rxgen;
+public import migemo_d.wordbuf;
+public import migemo_d.wordlist;
